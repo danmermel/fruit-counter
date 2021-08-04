1bf7138a728e0ca0c238f2a006e728c12c33227f
Daniel Mermelstein <danmermel@gmail.com>
2021-08-04 15:02:45 +0100

    vuejs installed

--- server.js
+++ server.js
@@ -6,9 +6,11 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello World!!');
-});
+app.use(express.static('public'))
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!!');
+// });
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
